951d681b270afa6429500046dad8fb091f9848b3
LNdame <ls20045@gmail.com>
2016-12-02 10:06:16 +0200

    before the change of api key and json to inqb8 acc

--- app/src/main/java/ansteph/com/beecab/view/extraaction/Disclaimer.java
+++ app/src/main/java/ansteph/com/beecab/view/extraaction/Disclaimer.java
@@ -9,23 +9,24 @@ import android.view.View;
 
 import ansteph.com.beecab.R;
 
-public class Credits extends AppCompatActivity {
+public class Disclaimer extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_credits);
+        setContentView(R.layout.activity_disclaimer);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-
-
-    public void onBackPres(View view)
-    {
-        onBackPressed();
-    }
 }
